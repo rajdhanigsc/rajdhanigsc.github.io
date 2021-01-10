a4f80db4c0d11a4dbe8b7e9e2a42836f1b7eae4a
ajeet <ajeetsinghparmar0@gmail.com>
2021-01-10 21:31:01 +0530

    Add scrol to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// spinner loader
+
 var myVar;
 
 function myFunction() {
@@ -7,4 +9,23 @@ function myFunction() {
 function showPage() {
   document.getElementById("spin").style.display = "none";
   document.getElementById("main").style.display = "block";
+}
+
+// scroll to top button
+
+let myButton = document.getElementById("mybtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
